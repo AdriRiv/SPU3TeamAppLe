809fd810bf69a0b0e1495a66cd6510f61fb55725
ChristianDzul <christiandzul092@gmail.com>
2021-08-06 11:31:15 -0500

    Function Len.h

--- len.h
+++ len.h
@@ -1,3 +1,45 @@
 #include <stdio.h>
 
-gato = 0;
+
+//GLOBAL VARIABLES//
+
+char*EncodedCountry = "************";
+char* country = "Mexico";
+int points;
+int currentPoint = 0 ;
+
+
+void  modifyVariables( int*AdressVariable){
+    *AdressVariable = points;
+    return;
+}
+
+char* encodeCountry( char*pais, int*AdressVariable ){
+    
+    srand(time(NULL));
+    int len = strlen(country);
+    
+    char* tempEncoded = (char*)malloc(sizeof(char)*len);   
+        for (int i = 0; i < len; i++)
+        {
+        tempEncoded[i] = '*';
+        }
+
+   
+    points = rand()%len;
+    int ocultar = rand()%len; 
+         for (int i = 0; i < points; i++)
+        {
+        int ocultar = rand()%len;
+        tempEncoded[ocultar] = country[ocultar];
+        }
+    EncodedCountry = tempEncoded;
+    int*CurrentPoint = &points;
+    modifyVariables(&points);
+
+    printf("Country Name: %s\n", EncodedCountry);
+    printf("Current Points:%d\n", points); 
+}
+
+
+
